81b5c6069dca3068f0e9cb9a90d21cfc16928759
Marcin Paul <megamen1285@gmail.com>
2018-06-09 00:32:59 +0200

    Wyświetlenie punktów na obrazie głębi

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -7,7 +7,10 @@
 #include <opencv2/core/core.hpp>
 #include <string>>
 
+
+
 using namespace cv;
+using namespace std;
 #define C_DEPTH_STREAM 0
 #define C_COLOR_STREAM 1
 #define C_NUM_STREAMS 2
@@ -24,16 +27,19 @@ const float calibrationSquareDimension = 0.025f; //meters
 
 int main()
 {
+	
 	while (mode == 0) {
 		int wybor=0;
 		std::cout << "Wpisz odpowiedni numer" << std::endl;
 		std::cout << "1 - Zapisz zdjêcia z kamery kinecta oraz zwyklej kamery" << std::endl;
 		std::cout << "2 - Kalibracja zapisanych zdjêæ" << std::endl;
 		std::cin >> wybor;
+		cin.clear(); cin.ignore(INT_MAX, '\n');
 		switch (wybor) {
-		case 1: mode = 1; break;
-		case 2: mode = 2; break;
-		default: mode = 0;
+		case 1:   mode = 1; break;
+		case 2:  mode = 2; break;
+		case 3:  mode = 3; break;
+		default: ;
 		}
 	}
 	while (mode == 1)
@@ -105,7 +111,7 @@ int main()
 			std::cout << openni::OpenNI::getExtendedError() << std::endl;
 			return false;
 		}
-		
+		int i = 1;
 		m_streams = new openni::VideoStream*[C_NUM_STREAMS];
 		m_streams[C_COLOR_STREAM] = &m_color;
 		m_streams[C_DEPTH_STREAM] = &m_depth;
@@ -119,7 +125,6 @@ int main()
 			cap >> frame; // get a new frame from camera
 			cv::imshow("Color2", frame);
 
-			
 			bool colorCaptured = false;
 			bool depthCaptured = false;
 			if (m_colorStreamStatus & m_depthStreamStatus)
@@ -140,7 +145,6 @@ int main()
 						bufferImage.create(m_colorFrame.getHeight(), m_colorFrame.getWidth(), CV_8UC3);
 						bufferImage.data = (uchar*)m_colorFrame.getData();
 						m_colorTimeStamp = m_colorFrame.getTimestamp() >> 16;
-						std::cout << "Color Timestamp: " << m_colorTimeStamp << std::endl;
 						colorCaptured = true;
 						cv::cvtColor(bufferImage, m_colorImage, CV_BGR2RGB);
 						break;
@@ -149,17 +153,12 @@ int main()
 						m_depthImage.create(m_depthFrame.getHeight(), m_depthFrame.getWidth(), CV_16UC1);
 						m_depthImage.data = (uchar*)m_depthFrame.getData();
 						m_depthTimeStamp = m_depthFrame.getTimestamp() >> 16;
-						std::cout << "Depth Timestamp: " << m_depthTimeStamp << std::endl;
 						depthCaptured = true;
 					default:
 						break;
 					}
 				}
-			/*	Vec.push_back(m_colorImage);
-				Vec.push_back(m_depthImage);
-				bufferImage.release();
-				dataStream = Vec;
-				std::cout << Vec.size() << std::endl;*/
+
 
 			}
 			else
@@ -172,79 +171,49 @@ int main()
 			dataStream.push_back(m_colorImage);
 
 			bufferImage.release();
-			std::cout << dataStream.size() << std::endl;
+			//std::cout << dataStream.size() << std::endl;
 			cv::imshow("Color", dataStream[C_COLOR_STREAM]);
 			cv::imshow("Depth", dataStream[C_DEPTH_STREAM]); 
 
+			string nazwa_kin = "Zdjecia/Kinect/img";
+			string nazwa_dep = "Zdjecia/Depth/img";
+			string nazwa_kol = "Zdjecia/Kamera/img";
+
+		
 			c = cv::waitKey(10);
-			if (c == '1')
+			if(i<=30 && c==83)
 			{
-				cv::namedWindow("Img_Kinect", CV_WINDOW_AUTOSIZE);
-				cv::namedWindow("Img_Kamera", CV_WINDOW_AUTOSIZE);
-				cv::imwrite("Zdjecia/Kinect/img1.jpg", dataStream[C_COLOR_STREAM]);
-				cv::imwrite("Zdjecia/Kamera/img1.jpg", frame);
-				cv::imshow("Img_Kinect", dataStream[C_COLOR_STREAM]);
-				cv::imshow("Img_Kamera", frame);
+				
+					string temp_kin = nazwa_kin + std::to_string(i) + ".jpg";
+					string temp_dep = nazwa_dep + std::to_string(i) + ".jpg";
+					string temp_kol = nazwa_kol + std::to_string(i) + ".jpg";
+
+					printf("Zapisano nr %d\n",i);
 
 
+					cv::imshow("Img_Kinect", dataStream[C_COLOR_STREAM]);
+					cv::imshow("Img_Kamera", frame);
+					cv::imshow("Img_Depth", dataStream[C_DEPTH_STREAM]);
+
+					cv::imwrite(temp_kin, dataStream[C_COLOR_STREAM]);
+					cv::imwrite(temp_dep, dataStream[C_DEPTH_STREAM]);
+					cv::imwrite(temp_kol, frame);
+					i++;
+
 			}
-			if (c == '2')
-			{
-				cv::imwrite("Zdjecia/Kinect/img2.jpg", dataStream[C_COLOR_STREAM]);
-				cv::imwrite("Zdjecia/Kamera/img2.jpg", frame);
-				cv::imshow("Img_Kinect", dataStream[C_COLOR_STREAM]);
-				cv::imshow("Img_Kamera", frame);
-			}
-			if (c == '3')
-			{
-				cv::imwrite("Zdjecia/Kinect/img3.jpg", dataStream[C_COLOR_STREAM]);
-				cv::imwrite("Zdjecia/Kamera/img3.jpg", frame);
-				cv::imshow("Img_Kinect", dataStream[C_COLOR_STREAM]);
-				cv::imshow("Img_Kamera", frame);
-			}
-			if (c == '4')
-			{
-				cv::imwrite("Zdjecia/Kinect/img4.jpg", dataStream[C_COLOR_STREAM]);
-				cv::imwrite("Zdjecia/Kamera/img4.jpg", frame);
-				cv::imshow("Img_Kinect", dataStream[C_COLOR_STREAM]);
-				cv::imshow("Img_Kamera", frame);
-			}
-			if (c == '5')
-			{
-				cv::imwrite("Zdjecia/Kinect/img5.jpg", dataStream[C_COLOR_STREAM]);
-				cv::imwrite("Zdjecia/Kamera/img5.jpg", frame);
-				cv::imshow("Img_Kinect", dataStream[C_COLOR_STREAM]);
-				cv::imshow("Img_Kamera", frame);
-			}
-			if (c == '6')
-			{
-				cv::imwrite("Zdjecia/Kinect/img6.jpg", dataStream[C_COLOR_STREAM]);
-				cv::imwrite("Zdjecia/Kamera/img6.jpg", frame);
-				cv::imshow("Img_Kinect", dataStream[C_COLOR_STREAM]);
-				cv::imshow("Img_Kamera", frame);
-			}
-			if (c == '7')
-			{
-				cv::imwrite("Zdjecia/Kinect/img7.jpg", dataStream[C_COLOR_STREAM]);
-				cv::imwrite("Zdjecia/Kamera/img7.jpg", frame);
-				cv::imshow("Img_Kinect", dataStream[C_COLOR_STREAM]);
-				cv::imshow("Img_Kamera", frame);
-			}
-			if (c == '8')
-			{
-				cv::imwrite("Zdjecia/Kinect/img8.jpg", dataStream[C_COLOR_STREAM]);
-				cv::imwrite("Zdjecia/Kamera/img8.jpg", frame);
-				cv::imshow("Img_Kinect", dataStream[C_COLOR_STREAM]);
-				cv::imshow("Img_Kamera", frame);
-			}
+
 		}
+		
 		m_colorFrame.release();
 		m_depthFrame.release();
+		
 		m_depth.stop();
 		m_color.stop();
 		openni::OpenNI::shutdown();
 		m_device.close();
+		
 		mode = 0; 
+		
 	}
 	if (mode == 2)
 	{
@@ -258,6 +227,7 @@ int main()
 		vector<vector<Point3f>> Punkty_obiektu;
 		vector<vector<Point2f>> Punkty_obrazu;
 		vector<Point2f> katy;
+		vector<Point2f> katy_d;
 		int successes = 0;
 
 		vector<Point3f> obj;
@@ -266,50 +236,70 @@ int main()
 
 
 		string nazwa = "Zdjecia/Kinect/img";
+		string nazwa2 = "Zdjecia/Depth/img";
+
 		char c;
 	//	cv::namedWindow("Zdjecia", CV_WINDOW_AUTOSIZE);
-		Mat image[24];	
-		for (int i = 1; i <=24; i++)
+		Mat image[30], image_depth[30];
+		for (int i = 1; i <=30; i++)
 		{
 			string s = nazwa + std::to_string(i) + ".jpg";
 			image[i-1] = imread(s);
-		
+			string s_depth = nazwa2 + std::to_string(i) + ".jpg";
+			image_depth[i - 1] = imread(s_depth);
 			
 		}
 		cv::imshow("win1", image[0]); 
 		cv::waitKey(); 
 
-		for (int i =0; i<=24; i++ )
+		for (int i =0; i<=30; i++ )
 		{
-			Mat gray_image;
+			Mat gray_image, gray_imaged;
 			cvtColor(image[i], gray_image, CV_BGR2GRAY);
+			cvtColor(image_depth[i], gray_imaged, CV_BGR2GRAY);
+
 			bool found = findChessboardCorners(image[i], wielkosc_tablicy, katy, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
 
 			if (found)
 			{
+				int xs = 640, ys = 480, xs2 = 320, ys2 = 240;
 				cornerSubPix(gray_image, katy, Size(11, 11), Size(-1, -1), TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+				for (std::vector<Point2f>::iterator it = katy.begin(); it!= katy.end(); ++it)
+				{
+	
+					cv::Point2f a;
+					a.x = ((( (int)it->x + 10 - xs2) * 241) >> 8) + xs2;
+					a.y = (((  (int)it->y + 30 - ys2) * 240) >> 8) + ys2;
+					katy_d.push_back(a);
+
+				}
+				drawChessboardCorners(gray_imaged, wielkosc_tablicy, katy_d, found);
 				drawChessboardCorners(gray_image, wielkosc_tablicy, katy, found);
+
+				katy_d.clear();
 			}
 			cv::imshow("win1", image[i]);
-			cv::imshow("win2", gray_image);
-			string sciezka = "Zdj_szare_z_punktami/img" + std::to_string(i) + ".jpg";
+			cv::imshow("win2", gray_imaged);
+			cv::imshow("win3", gray_image);
+
+			string sciezka = "Zdj_szare_z_punktami/img" + std::to_string(i+1) + ".jpg";
 			imwrite(sciezka, gray_image);
 			int key = waitKey(0);
 
 			if (key == 27)
-
 				return 0;
-
-			if ( found != 0)
+			else
+	
+			if ( found != 0  )
 			{
-				Punkty_obrazu.push_back(katy);
+ 				Punkty_obrazu.push_back(katy);
 				Punkty_obiektu.push_back(obj);
 
-				printf("Snap stored!");
+				cout<<("Snap stored!");
 
 				successes++;
 
-				if (successes >= 24)
+				if (successes >= 30|| i==29)
 					break;
 			}
 			
@@ -323,9 +313,36 @@ int main()
 		fs << "D" << D;
 		fs << "punktyPionowo" << punktyPionowo;
 		fs << "punktyPoziomo" << punktyPoziomo;
-		printf("Kalibracja Skoñczona");
+		cout << ("Kalibracja Skoñczona");
 		
 		cv::waitKey();
 		return 0;
+	}
+	while (mode == 3)
+	{
+		Mat image_depth[30], image_kinect[30];
+
+		string nazwa = "Zdjecia/Depth/img";
+		string nazwa2 = "Zdj_szare_z_punktami/img";
+
+
+		for (int i = 1; i <= 30; i++)
+		{
+			string s_depth = nazwa + std::to_string(i) + ".jpg";
+			image_depth[i - 1] = imread(s_depth);
+			string s_kinect = nazwa2 + std::to_string(i) + ".jpg";
+			
+			image_kinect[i - 1] = imread(s_kinect);
+			int key = cv::waitKey();
+			if (key == 27)
+				return 0;
+
+		}
+		cv::destroyAllWindows();
+		int key = cv::waitKey();
+		mode = 0;
+		if (key == 27)
+			return 0;
+
 	}
 }
